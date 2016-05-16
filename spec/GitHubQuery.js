202818500c2fd3293c9a2b5ef41fc078ec4a4054
Mariah Tree <mariah.tree@gmail.com>
2016-05-16 15:23:37 -0500

    listed intended tests and placed lib file in 'browser' on package.json

--- spec/GitHubQuery.js
+++ spec/GitHubQuery.js
@@ -1,3 +1,28 @@
 describe("Github Query", function(){
-  
-})
+  var GitHubQuery = reqire("../lib/GitHubQuery.js");
+  var gitHubQuery;
+
+  beforeEach(function (){
+    gitHubQuery = new GitHubQuery();
+  });
+
+  it("Parses login", function (){
+
+  });
+
+  it("Parses name", function (){
+
+  });
+  it("Parses location", function (){
+
+  });
+  it("Parses number of repos", function (){
+
+  });
+  it("Parses avatar url", function (){
+
+  });
+  it("Parses html url", function (){
+
+  });
+});
